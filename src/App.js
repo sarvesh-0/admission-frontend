67f6f07915351c2d15e4c5c69e49af7ac1e8c7f9
sarvesh-0 <sarveshteware651@gmail.com>
2025-03-20 19:35:30 +0530

    changes in app.js

--- src/App.js
+++ src/App.js
@@ -16,9 +16,9 @@ function App() {
       <Navbar />
         <main className="main-content">
             <Routes>
-                <Route path="/" element={<AdmissionForm />} />
+                <Route path="/admissionform" element={<AdmissionForm />} />
                 <Route path="/report" element={<Report />} />
-                <Route path="/about" element={<AboutUs/>}/>
+                <Route path="/" element={<AboutUs/>}/>
                 <Route path="/view/:id" element={<ViewAdmissionForm />} />
                 <Route path="/edit/:id" element={<EditAdmissionForm />} /> {/* Add route for editing */}
             </Routes>
